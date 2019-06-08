d0d1402793c566c68c4eb3a1fb5be7d12c42088c
DwaraknathT <34276428+DwaraknathT@users.noreply.github.com>
2019-06-09 00:07:49 +0530

    fixed checkpoint issue

--- train.py
+++ train.py
@@ -18,9 +18,6 @@ from absl import flags
 import psutil
 import humanize
 import os
-import GPUtil as GPU
-GPUs = GPU.getGPUs()
-gpu = GPUs[0]
 
 from data_loader import get_dataset, get_gat_dataset, convert, preprocess_sentence
 from src.models import model_params, transformer, graph_attention_model, rnn_model
@@ -37,19 +34,14 @@ PARAMS_MAP = {
     "big": model_params.BIG_PARAMS,
 }
 
-def printm():
-    process = psutil.Process(os.getpid())
-    print("Gen RAM Free: " + humanize.naturalsize( psutil.virtual_memory().available ), " | Proc size: " + humanize.naturalsize( process.memory_info().rss))
-    print("GPU RAM Free: {0:.0f}MB | Used: {1:.0f}MB | Util {2:3.0f}% | Total {3:.0f}MB".format(gpu.memoryFree, gpu.memoryUsed, gpu.memoryUtil*100, gpu.memoryTotal))
-
 if __name__ == "__main__":
     args = get_args()
-    '''
+
     #set up dirs
-    if args.use_colab == False:
+    if args.use_colab != False:
         args.checkpoint_dir = 'ckpts/' if args.checkpoint_dir is None else args.checkpoint_dir
         output_file = 'results.txt'
-        if args.output_dir is None and not os.path.isdir('ckpts'):
+        if args.checkpoint_dir is None and not os.path.isdir('ckpts'):
             os.mkdir('ckpts')
     else:
         from google.colab import drive
@@ -57,24 +49,24 @@ if __name__ == "__main__":
         OUTPUT_DIR = '/content/gdrive/My Drive/ckpts'
         args.checkpoint_dir = OUTPUT_DIR
         output_file = OUTPUT_DIR + '/results.txt'
-        if not os.path.isdir(OUTPUT_DIR): os.mkdir(OUTPUT_DIR)'''
+        if not os.path.isdir(OUTPUT_DIR): os.mkdir(OUTPUT_DIR)
 
     if args.enc_type == 'gat':
         (dataset, BUFFER_SIZE, BATCH_SIZE, steps_per_epoch, 
         vocab_tgt_size, vocab_nodes_size, target_lang, max_length_targ) = get_gat_dataset(args)
 
         embedding = tf.keras.layers.Embedding(vocab_nodes_size, args.emb_dim) 
+
         model = graph_attention_model.GATModel(args, vocab_tgt_size, target_lang)
 
         optimizer = tf.train.AdamOptimizer()
         loss_object = tf.keras.losses.CategoricalCrossentropy()
-        """
+
         checkpoint_dir = args.checkpoint_dir
         checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
         checkpoint = tf.train.Checkpoint( optimizer=optimizer,
-                                            encoder= encoder,
-                                            decoder= decoder)
-        """
+                                            model=model)
+
         if args.epochs is not None:
             steps = args.epochs * steps_per_epoch
         else:
@@ -98,7 +90,7 @@ if __name__ == "__main__":
             predictions, dec_hidden, loss = model(adj, nodes, targ)
             eval_loss = (loss / int(targ.shape[1]))
             model.trainable = True
-            
+
             return eval_loss
 
         total_loss =0
@@ -124,12 +116,11 @@ if __name__ == "__main__":
                 batch_loss = train_step(adj, nodes, targ)
                 print('Step {} Train Loss{:.4f} \n'.format(batch,
                                                         batch_loss.numpy()))
-            printm()
-            """
+
             if batch % args.checkpoint == 0:
                 checkpoint.save(file_prefix = checkpoint_prefix)
             print('Time {} \n'.format(time.time() - start))
-            """
+
     else:
         dataset, BUFFER_SIZE, BATCH_SIZE,\
         steps_per_epoch, vocab_inp_size, vocab_tgt_size, target_lang = get_dataset(args)
@@ -138,20 +129,18 @@ if __name__ == "__main__":
         loss_object = tf.keras.losses.CategoricalCrossentropy()
         model = rnn_model.RNNModel(vocab_inp_size, vocab_tgt_size, target_lang, args)
         enc_hidden = model.encoder.initialize_hidden_state()
-        """
+
         checkpoint_dir = args.checkpoint_dir
         checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
         checkpoint = tf.train.Checkpoint(optimizer=optimizer,
-                                         encoder=encoder,
-                                         decoder=decoder)
-        """
+                                         model=model)
+
         def loss_function(real, pred):
             mask = tf.math.logical_not(tf.math.equal(real, 0))
             loss_ = loss_object(real, pred)
 
             mask = tf.cast(mask, dtype=loss_.dtype)
             loss_ *= mask
-
             return tf.reduce_mean(loss_)
 
         def train_step(inp, targ, enc_hidden):
@@ -167,15 +156,13 @@ if __name__ == "__main__":
 
             return batch_loss
 
-
         def eval_step(inp, trg, enc_hidden):
             eval_loss = 0 
             model.trainable = False
 
             predictions, dec_hidden, eval_loss = model(inp, trg, enc_hidden)
             eval_loss = (eval_loss / int(targ.shape[1]))
-            
-            model.trainable = True 
+            model.trainable = True
 
             return eval_loss
 
@@ -187,8 +174,8 @@ if __name__ == "__main__":
             else:
                 batch_loss = train_step(inp, targ, enc_hidden)
                 print('Step {} Batch Loss {:.4f} \n'.format(batch,batch_loss.numpy()))
-            """
+
             if batch % args.checkpoint == 0:
                 checkpoint.save(file_prefix = checkpoint_prefix)
             print('Time {} \n'.format(time.time() - start))
-            """
+
