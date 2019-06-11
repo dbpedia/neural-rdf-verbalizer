5b2b08c428d59f1cbc5c00b65b71d85a615fef91
DwaraknathT <34276428+DwaraknathT@users.noreply.github.com>
2019-06-11 20:36:52 +0530

    Update decoders.py

--- src/layers/decoders.py
+++ src/layers/decoders.py
@@ -5,7 +5,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import tensorflow as tf
-from src.layers.attention_layer import BahdanauAttention
+from src.layers.attention_layer import BahdanauAttention, MultiHeadAttention
+from src.utils.model_utils import point_wise_feed_forward_network
 import abc
 from collections import namedtuple
 import six
@@ -16,7 +17,7 @@ class RNNDecoder(tf.keras.Model):
     self.batch_sz = batch_sz
     self.dec_units = dec_units
     self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-    self.gru = tf.keras.layers.GRU(self.dec_units,
+    self.gru = tf.keras.layers.CuDNNGRU(self.dec_units,
                                    return_sequences=True,
                                    return_state=True,
                                    recurrent_initializer='glorot_uniform')
@@ -45,3 +46,40 @@ class RNNDecoder(tf.keras.Model):
     x = self.fc(output)
 
     return x, state, attention_weights
+
+
+class TransformerDecoder(tf.keras.layers.Layer):
+  def __init__(self, d_model, num_heads, dff, rate=0.1):
+    super(TransformerDecoder, self).__init__()
+
+    self.mha1 = MultiHeadAttention(d_model, num_heads)
+    self.mha2 = MultiHeadAttention(d_model, num_heads)
+
+    self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+    self.layernorm1 = tf.contrib.layers.layer_norm
+    self.layernorm2 = tf.contrib.layers.layer_norm
+    self.layernorm3 = tf.contrib.layers.layer_norm
+
+    self.dropout1 = tf.keras.layers.Dropout(rate)
+    self.dropout2 = tf.keras.layers.Dropout(rate)
+    self.dropout3 = tf.keras.layers.Dropout(rate)
+
+  def call(self, x, enc_output, training,
+           look_ahead_mask, padding_mask):
+    # enc_output.shape == (batch_size, input_seq_len, d_model)
+
+    attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)  # (batch_size, target_seq_len, d_model)
+    attn1 = self.dropout1(attn1, training=training)
+    out1 = self.layernorm1(attn1 + x)
+
+    attn2, attn_weights_block2 = self.mha2(
+      enc_output, enc_output, out1, padding_mask)  # (batch_size, target_seq_len, d_model)
+    attn2 = self.dropout2(attn2, training=training)
+    out2 = self.layernorm2(attn2 + out1)  # (batch_size, target_seq_len, d_model)
+
+    ffn_output = self.ffn(out2)  # (batch_size, target_seq_len, d_model)
+    ffn_output = self.dropout3(ffn_output, training=training)
+    out3 = self.layernorm3(ffn_output + out2)  # (batch_size, target_seq_len, d_model)
+
+    return out3, attn_weights_block1, attn_weights_block2
