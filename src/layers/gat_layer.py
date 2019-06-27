21ae4adf37c3953c32f2ee44754721c1d0ab4d18
DwaraknathT <dwarakasharma@gmail.com>
2019-06-27 22:27:34 +0530

    redone gat

--- src/layers/gat_layer.py
+++ src/layers/gat_layer.py
@@ -26,25 +26,18 @@ class GraphAttentionLayer (tf.keras.layers.Layer):
         self.attn_kernels = []
 
         self.node_layer = tf.keras.layers.Dense(dff)
-        self.edge_layer = tf.keras.layers.Dense(dff)
         self.lrelu = tf.keras.layers.LeakyReLU()
         self.dropout = tf.keras.layers.Dropout(rate)
 
         for head in range(self.num_heads):
-            kernel = self.add_weight(shape=(self.in_dim, self.out_dim),
-                                     initializer='glorot_uniform',
-                                     name='kernel_{}'.format(head))
+            kernel = tf.keras.layers.Dense(self.out_dim, activation=tf.keras.activations.relu,
+                                           kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform')
             self.kernels.append(kernel)
-            bias = self.add_weight(shape=(self.out_dim, ),
-                                   initializer='glorot_uniform',
-                                   name='bias_{}'.format(head))
             # Attention kernels
-            attn_kernel_self = self.add_weight(shape=(self.out_dim, 1),
-                                               initializer='glorot_uniform',
-                                               name='attn_kernel_self_{}'.format(head))
-            attn_kernel_neighs = self.add_weight(shape=(self.out_dim, 1),
-                                                 initializer='glorot_uniform',
-                                                 name='attn_kernel_neigh_{}'.format(head))
+            attn_kernel_self = tf.keras.layers.Dense(1,
+                                           kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform')
+            attn_kernel_neighs = tf.keras.layers.Dense(1,
+                                           kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform')
             self.attn_kernels.append([attn_kernel_self, attn_kernel_neighs])
 
     def call(self, nodes, adj, num_heads, training, mask=None):
@@ -56,15 +49,13 @@ class GraphAttentionLayer (tf.keras.layers.Layer):
             kernel = self.kernels[head]
             attention_kernel = self.attn_kernels[head]
 
-            features = tf.keras.backend.dot(inputs, kernel)
-            attn_for_self = tf.keras.backend.dot(features, attention_kernel[0])
-            attn_for_neighs = tf.keras.backend.dot(features, attention_kernel[1])
+            features = kernel(inputs)
+            attn_for_self = attention_kernel[0](features)
+            attn_for_neighs = attention_kernel[1](features)
             # Attention head a(Wh_i, Wh_j) = a^T [[Wh_i], [Wh_j]]
 
-            dense = tf.add(attn_for_neighs, attn_for_self)
-            dense = tf.broadcast_to(dense, shape=adj.shape)
+            dense = tf.matmul(attn_for_self, attn_for_neighs, transpose_b=True)
             dense = self.lrelu(dense)
-
             # Mask values before activation (Vaswani et al., 2017)
             mask_local = -10e9 * (1.0 - adj)
             dense += mask_local
