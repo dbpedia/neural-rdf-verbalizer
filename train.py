a21a8579969b749a16aeff3e2b6d275340e4d113
DwaraknathT <dwarakasharma@gmail.com>
2019-06-28 11:15:42 +0530

    scheduler fix in gat_rnn

--- train.py
+++ train.py
@@ -134,8 +134,6 @@ if __name__ == "__main__":
                         print("Saving checkpoint \n")
                     print('Time {} \n'.format(time.time() - start))
                     pbar.update(1)
-                if args.decay is not None:
-                    optimizer._lr = optimizer._lr * args.decay_rate ** (epoch // 1)
 
     elif args.enc_type == 'rnn' and args.dec_type =="rnn":
       
