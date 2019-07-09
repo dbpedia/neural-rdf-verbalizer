263a78997feae3e17accbceb3c147b6098070dc4
DwaraknathT <dwarakasharma@gmail.com>
2019-07-09 12:51:22 +0530

    added LR

--- train_single.py
+++ train_single.py
@@ -358,7 +358,7 @@ if __name__ == "__main__":
             optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.98,
                                                epsilon=1e-9)
         else:
-            optimizer = tf.train.AdamOptimizer(beta1=0.9, beta2=0.98,
+            optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate, beta1=0.9, beta2=0.98,
                                                epsilon=1e-9)
         step =0
 
