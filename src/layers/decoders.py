57bcb6c6147180bd8d327ea1d061a5192db8830a
DwaraknathT <dwarakasharma@gmail.com>
2019-07-10 10:04:04 +0530

    Added CuDnnGRU

--- src/layers/decoders.py
+++ src/layers/decoders.py
@@ -14,9 +14,10 @@ class RNNDecoder(tf.keras.Model):
     self.batch_sz = batch_sz
     self.dec_units = dec_units
     self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-    self.gru = tf.keras.layers.GRU(self.dec_units,
+    self.forward_gru = tf.keras.layers.CuDNNGRU(self.dec_units,
                                    return_sequences=True,
                                    return_state=True,
+                                   go_backwards=True,
                                    recurrent_initializer='glorot_uniform')
     self.fc = tf.keras.layers.Dense(vocab_size)
 
@@ -35,7 +36,7 @@ class RNNDecoder(tf.keras.Model):
     x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
     # passing the concatenated vector to the GRU
-    output, state = self.gru(x)
+    output, state = self.forward_gru(x)
 
     # output shape == (batch_size * 1, hidden_size)
     output = tf.reshape(output, (-1, output.shape[2]))
