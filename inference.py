c54b5f053a869ab29172207c343b38c0b885c77d
DwaraknathT <dwarakasharma@gmail.com>
2019-07-17 11:55:15 +0530

    Inference fix

--- inference.py
+++ inference.py
@@ -104,6 +104,7 @@ def process_gat_sentence(line, src_lang, target_lang):
     triple_list = line.split('< TSP >')
     for l in triple_list:
         l = l.strip().split(' | ')
+        l = [target_lang + ' ' + x for x in l]
         g.add_edge(l[0], l[1], label='A_ZERO')
         g.add_edge(l[1], l[2], label='A_ONE')
     node_list = list(g.nodes())
