822ca05dc81694e630ce9b69fbd7f416a7ca4721
DwaraknathT <34276428+DwaraknathT@users.noreply.github.com>
2019-08-04 02:13:09 -0700

    Update model_utils.py

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -26,6 +26,8 @@ def unicode_to_ascii(s):
 
 def PreProcessSentence(w, lang):
     w = unicode_to_ascii(w.lower().strip())
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
     w = w.rstrip().strip()
     w = 'start ' + w + ' end'
 
