51a2c484c428969241daf2fd683c598d2c647ddb
DwaraknathT <dwarakasharma@gmail.com>
2019-08-06 07:21:23 +0530

    more exp with vocab

--- train_single.py
+++ train_single.py
@@ -443,6 +443,8 @@ if __name__ == "__main__":
                     sentence = ""
                     #sentence = (tgt_vocab.DecodeIds(list(pred[0][i])))
                     for w in list(pred[0][i]):
+                        if w ==0:
+                            continue
                         if tgt_vocab.index_word[w] == 'end':
                             break
                         else:
@@ -473,6 +475,8 @@ if __name__ == "__main__":
                 for i in range(len(pred[0])):
                     #sentence = (tgt_vocab.DecodeIds(list(pred[0][i])))
                     for w in list(pred[0][i]):
+                        if w==0:
+                            continue
                         if tgt_vocab.index_word[w] == 'end':
                             break
                         else:
