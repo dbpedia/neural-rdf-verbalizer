3609e412aefe4585642fc90d71d2efc3ec59770b
dwarak <dwarakasharma@gmail.com>
2019-12-18 11:11:40 +0530

    cleaning preprocess.py

--- commands.sh
+++ commands.sh
@@ -1,10 +1,8 @@
-python3 train_single.py --train_path 'data/processed_graphs/rus/gat/reif_train' \
---eval_path 'data/processed_graphs/rus/gat/reif_eval' \
---test_path 'data/processed_graphs/rus/gat/reif_test' \
---src_vocab 'vocabs/gat/rus/reif_src_vocab' --tgt_vocab 'vocabs/gat/rus/train_tgt.model' \
---batch_size 1 --enc_type gat --dec_type transformer --model gat --vocab_size 16000 \
---emb_dim 16 --hidden_size 16  --filter_size 16 --use_bias True --beam_size 5 \
---beam_alpha 0.1  --enc_layers 1 --dec_layers 1 --num_heads 1 --use_edges False \
---steps 1 --eval_steps 1000 --checkpoint 1000 --alpha 0.2 --dropout 0.2 --resume False \
---reg_scale 0.0 --decay True --decay_steps 5000 --lang rus --use_colab True --opt reif \
---eval 'data/processed_data/rus/eval_src' --eval_ref 'data/tokenized/rus/eval_tgt'
+python3 preprocess.py --train_src 'data/processed_data/eng/train_src' \
+--train_tgt 'data/processed_data/eng/train_tgt' \
+--eval_src 'data/processed_data/eng/eval_src' \
+--eval_tgt 'data/processed_data/eng/eval_tgt' \
+--test_src 'data/processed_data/eng/test_src' \
+--spl_sym 'data/processed_data/metadata/special_symbols' \
+--model gat --lang eng --sentencepiece True \
+--vocab_size 16000 --max_seq_len 100 --sentencepiece_model 'bpe'
--- preprocess.py
+++ preprocess.py
@@ -21,7 +21,7 @@ import pickle
 import sentencepiece as spm
 import tensorflow as tf
 
-from src.utils.PreprocessingUtils import PreProcess, PreProcessRolesModel
+from src.utils.PreprocessingUtils import PreProcess
 from src.utils.model_utils import PreProcessSentence
 
 parser = argparse.ArgumentParser(description="preprocessor parser")
@@ -39,8 +39,6 @@ parser.add_argument(
   '--test_src', type=str, required=False, help='Path to test source file')
 parser.add_argument(
   '--model', type=str, required=True, help='Preprocess for GAT model or seq2seq model')
-parser.add_argument(
-  '--opt', type=str, required=False, help='Role processing or Reification: role, reif ')
 parser.add_argument(
   '--use_colab', type=bool, required=False, help='Use colab or not')
 parser.add_argument(
@@ -60,136 +58,84 @@ if __name__ == '__main__':
   os.makedirs(('vocabs/gat/' + args.lang), exist_ok=True)
 
   if args.model == 'gat':
-    if args.opt == 'role':
-      print('Building the dataset...')
-      train_adj, train_nodes, train_roles, train_edges = PreProcessRolesModel(args.train_src)
-      eval_adj, eval_nodes, eval_roles, eval_edges = PreProcessRolesModel(args.eval_src)
+    train_nodes, train_labels, train_node1, train_node2 = PreProcess(args.train_src, args.lang)
+    eval_nodes, eval_labels, eval_node1, eval_node2 = PreProcess(args.eval_src, args.lang)
+    test_nodes, test_labels, test_node1, test_node2 = PreProcess(args.test_src, args.lang)
 
-      print('Building the Vocab file... ')
-      train_tgt = io.open(args.train_tgt, encoding='UTF-8').read().strip().split('\n')
-      train_tgt = [(PreProcessSentence(w, args.lang)) for w in train_tgt]
-      eval_tgt = io.open(args.eval_tgt, encoding='UTF-8').read().strip().split('\n')
-      eval_tgt = [(PreProcessSentence(w, args.lang)) for w in eval_tgt]
+    # Build and save the vocab
+    print('Building the  Source Vocab file... ')
+    train_tgt = io.open(args.train_tgt, encoding='UTF-8').read().strip().split('\n')
+    train_tgt = [PreProcessSentence(w, args.sentencepiece, args.lang) for w in train_tgt]
+    # vocab_train_tgt = [tokenizer(w) for w in train_tgt]
+    eval_tgt = io.open(args.eval_tgt, encoding='UTF-8').read().strip().split('\n')
+    eval_tgt = [PreProcessSentence(w, args.sentencepiece, args.lang) for w in eval_tgt]
 
-      # Create the train and test sets
-      train_input = list(zip(train_adj, train_nodes, train_roles, train_edges))
-      eval_input = list(zip(eval_adj, eval_nodes, eval_roles, eval_edges))
-      train_set = list(zip(train_input, train_tgt))
-      eval_set = list(zip(eval_input, eval_tgt))
-      print('Lengths of train and eval sets {} {}'.format(len(train_set), len(eval_set)))
+    vocab = tf.keras.preprocessing.text.Tokenizer(filters='')
+    vocab.fit_on_texts(train_nodes)
+    vocab.fit_on_texts(train_labels)
+    vocab.fit_on_texts(train_node1)
+    vocab.fit_on_texts(train_node2)
+    vocab.fit_on_texts(eval_nodes)
+    vocab.fit_on_texts(eval_labels)
+    vocab.fit_on_texts(eval_node1)
+    vocab.fit_on_texts(eval_node2)
 
-      vocab = tf.keras.preprocessing.text.Tokenizer(filters='')
-      vocab.fit_on_texts(train_tgt)
-      # vocab.fit_on_texts(eval_tgt)
-      vocab.fit_on_texts(train_nodes)
-      vocab.fit_on_texts(train_edges)
-      vocab.fit_on_texts(train_roles)
-      # vocab.fit_on_texts(eval_nodes)
-      # vocab.fit_on_texts(eval_edges)
-      # vocab.fit_on_texts(eval_roles)
-
-      if args.use_colab is not None:
-        from google.colab import drive
-
-        drive.mount('/content/gdrive', force_remount=True)
-        OUTPUT_DIR = '/content/gdrive/My Drive/data/processed_graphs/' + args.lang + '/' + args.model
-        if not os.path.isdir(OUTPUT_DIR):
-          os.makedirs(OUTPUT_DIR)
-      else:
-        OUTPUT_DIR = 'data/processed_graphs/' + args.lang + '/' + args.model
-        if not os.path.isdir(OUTPUT_DIR):
-          os.makedirs(OUTPUT_DIR)
-
-      with open(OUTPUT_DIR + '/' + args.opt + '_train', 'wb') as fp:
-        pickle.dump(train_set, fp)
-      with open(OUTPUT_DIR + '/' + args.opt + '_eval', 'wb') as fp:
-        pickle.dump(eval_set, fp)
-      print('Vocab Size : {}\n'.format(len(vocab.word_index)))
-
-      os.makedirs(('vocabs/gat/' + args.lang), exist_ok=True)
-      with open(('vocabs/gat/' + args.lang + '/' + args.opt + '_vocab'), 'wb+') as fp:
-        pickle.dump(vocab, fp)
-
-    elif args.opt == 'reif':
-
-      train_nodes, train_labels, train_node1, train_node2 = PreProcess(args.train_src, args.lang)
-      eval_nodes, eval_labels, eval_node1, eval_node2 = PreProcess(args.eval_src, args.lang)
-      test_nodes, test_labels, test_node1, test_node2 = PreProcess(args.test_src, args.lang)
-
-      # Build and save the vocab
-      print('Building the  Source Vocab file... ')
-      train_tgt = io.open(args.train_tgt, encoding='UTF-8').read().strip().split('\n')
-      train_tgt = [PreProcessSentence(w, args.sentencepiece, args.lang) for w in train_tgt]
-      # vocab_train_tgt = [tokenizer(w) for w in train_tgt]
-      eval_tgt = io.open(args.eval_tgt, encoding='UTF-8').read().strip().split('\n')
-      eval_tgt = [PreProcessSentence(w, args.sentencepiece, args.lang) for w in eval_tgt]
-
-      vocab = tf.keras.preprocessing.text.Tokenizer(filters='')
-      vocab.fit_on_texts(train_nodes)
-      vocab.fit_on_texts(train_labels)
-      vocab.fit_on_texts(train_node1)
-      vocab.fit_on_texts(train_node2)
-      vocab.fit_on_texts(eval_nodes)
-      vocab.fit_on_texts(eval_labels)
-      vocab.fit_on_texts(eval_node1)
-      vocab.fit_on_texts(eval_node2)
-
-      if args.sentencepiece == 'True':
-        spm.SentencePieceTrainer.Train('--input=' + args.train_tgt + ',' + args.eval_tgt + ' \
+    if args.sentencepiece == 'True':
+      spm.SentencePieceTrainer.Train('--input=' + args.train_tgt + ',' + args.eval_tgt + ' \
                                                 --model_prefix=vocabs/' + args.model + '/' + args.lang + '/train_vocab \
                                                 --vocab_size=' + str(
-          args.vocab_size) + ' --character_coverage=1.0 '
-                             '--model_type=' + args.sentencepiece_model +
-                                       ' --max_sentencepiece_length=' + str(args.max_seq_len))
-        sp = spm.SentencePieceProcessor()
-        sp.load('vocabs/' + args.model + '/' + args.lang + '/train_vocab.model')
-        print('Sentencepiece vocab size {}'.format(sp.get_piece_size()))
+        args.vocab_size) + ' --character_coverage=1.0 '
+                           '--model_type=' + args.sentencepiece_model +
+                                     ' --max_sentencepiece_length=' + str(args.max_seq_len))
+      sp = spm.SentencePieceProcessor()
+      sp.load('vocabs/' + args.model + '/' + args.lang + '/train_vocab.model')
+      print('Sentencepiece vocab size {}'.format(sp.get_piece_size()))
 
-        target_vocab = sp
-      else:
-        vocab.fit_on_texts(train_tgt)
-        vocab.fit_on_texts(eval_tgt)
+      target_vocab = sp
+    else:
+      vocab.fit_on_texts(train_tgt)
+      vocab.fit_on_texts(eval_tgt)
 
-      print('Vocab Size : {}\n'.format(len(vocab.word_index)))
+    print('Vocab Size : {}\n'.format(len(vocab.word_index)))
 
-      train_input = list(zip(train_nodes, train_labels, train_node1, train_node2))
-      eval_input = list(zip(eval_nodes, eval_labels, eval_node1, eval_node2))
-      test_input = list(zip(test_nodes, test_labels, test_node1, test_node2))
-      train_set = list(zip(train_input, train_tgt))
-      eval_set = list(zip(eval_input, eval_tgt))
-      print('Train and eval dataset size : {} {} '.format(len(train_set), len(eval_set)))
+    train_input = list(zip(train_nodes, train_labels, train_node1, train_node2))
+    eval_input = list(zip(eval_nodes, eval_labels, eval_node1, eval_node2))
+    test_input = list(zip(test_nodes, test_labels, test_node1, test_node2))
+    train_set = list(zip(train_input, train_tgt))
+    eval_set = list(zip(eval_input, eval_tgt))
+    print('Train and eval dataset size : {} {} '.format(len(train_set), len(eval_set)))
 
-      if args.use_colab is not None:
-        from google.colab import drive
+    if args.use_colab is not None:
+      from google.colab import drive
 
-        drive.mount('/content/gdrive', force_remount=True)
-        OUTPUT_DIR = '/content/gdrive/My Drive/data/processed_graphs/' + args.lang + '/' + args.model
-        if not os.path.isdir(OUTPUT_DIR):
-          os.makedirs(OUTPUT_DIR)
-        # save the vocab file
-        os.makedirs(('vocabs/gat/' + args.lang), exist_ok=True)
-        with open(('vocabs/gat/' + args.lang + '/' + args.opt + '_src_vocab'), 'wb+') as fp:
-          pickle.dump(vocab, fp)
+      drive.mount('/content/gdrive', force_remount=True)
+      OUTPUT_DIR = '/content/gdrive/My Drive/data/processed_graphs/' + args.lang + '/' + args.model
+      if not os.path.isdir(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+      # save the vocab file
+      os.makedirs(('vocabs/gat/' + args.lang), exist_ok=True)
+      with open(('vocabs/gat/' + args.lang + '/'+ '_src_vocab'), 'wb+') as fp:
+        pickle.dump(vocab, fp)
 
-      else:
-        OUTPUT_DIR = 'data/processed_graphs/' + args.lang + '/' + args.model
-        if not os.path.isdir(OUTPUT_DIR):
-          os.makedirs(OUTPUT_DIR)
-        # save the vocab file
-        os.makedirs(('vocabs/gat/' + args.lang), exist_ok=True)
-        with open(('vocabs/gat/' + args.lang + '/' + args.opt + '_src_vocab'), 'wb+') as fp:
-          pickle.dump(vocab, fp)
+    else:
+      OUTPUT_DIR = 'data/processed_graphs/' + args.lang + '/' + args.model
+      if not os.path.isdir(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+      # save the vocab file
+      os.makedirs(('vocabs/gat/' + args.lang), exist_ok=True)
+      with open(('vocabs/gat/' + args.lang + '/' + '_src_vocab'), 'wb+') as fp:
+        pickle.dump(vocab, fp)
 
-      print('Vocab file saved !\n')
-      print('Preparing the Graph Network datasets...')
+    print('Vocab file saved !\n')
+    print('Preparing the Graph Network datasets...')
 
-      with open(OUTPUT_DIR + '/' + args.opt + '_train', 'wb') as fp:
-        pickle.dump(train_set, fp)
-      with open(OUTPUT_DIR + '/' + args.opt + '_eval', 'wb') as fp:
-        pickle.dump(eval_set, fp)
-      with open(OUTPUT_DIR + '/' + args.opt + '_test', 'wb') as fp:
-        pickle.dump(test_input, fp)
-      print('Dumped the train, eval and test datasets.')
+    with open(OUTPUT_DIR + '/' + '_train', 'wb') as fp:
+      pickle.dump(train_set, fp)
+    with open(OUTPUT_DIR + '/' + '_eval', 'wb') as fp:
+      pickle.dump(eval_set, fp)
+    with open(OUTPUT_DIR + '/' + '_test', 'wb') as fp:
+      pickle.dump(test_input, fp)
+    print('Dumped the train, eval and test datasets.')
 
   else:
     # Train the vocabs
